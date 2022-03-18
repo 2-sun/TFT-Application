16a5df1eb3117ac8da76f9b4eb70169014f77009
2-sun <qwerkevin1103@gmail.com>
2022-03-18 21:11:02 +0900

    Login System

---
+++ TFT/LoginWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System.Threading;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TFT
+{
+    /// <summary>
+    /// Window1.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        bool logining = false;
+
+        public LoginWindow()
+        {
+
+            InitializeComponent();
+
+            KeyGesture loginKeyGesture = new KeyGesture(Key.Enter);
+
+            KeyBinding loginKeyBinding = new KeyBinding(ApplicationCommands.Save, loginKeyGesture);
+
+            InputBindings.Add(loginKeyBinding);
+        
+        }
+
+        private void ClickLoginButton(object sender, RoutedEventArgs e)
+        {
+
+            string id = txtBox_id.Text;
+            string pw = txtBox_pw.Password;
+
+            btn_login.IsEnabled = false;
+
+            Login(id, pw);
+
+            btn_login.IsEnabled = true;
+            
+        }
+
+        private void CommandCanExecute(object sender, CanExecuteRoutedEventArgs e)
+        {
+
+            if (logining)
+            {
+
+                e.CanExecute = false;
+
+            }
+            else
+            {
+
+                e.CanExecute = true;
+
+            }
+
+
+        }
+
+        private void CommandExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+
+            string id = txtBox_id.Text;
+            string pw = txtBox_pw.Password;
+
+            Login(id, pw);
+
+        }
+
+
+        private void Login(string id, string pw)
+        {
+
+            logining = true;
+
+            if (id == "" || pw == "")
+            {
+
+                MessageBox.Show("id/pw cannot be empty", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+
+                logining = false;
+
+                return;
+
+            }
+
+            try
+            {
+
+                Player player = new Player(id, pw);
+
+                new MainWindow().Show();
+
+                this.Close();
+
+                logining = false;
+
+            }
+            catch (PlayerException)
+            {
+
+                if (MessageBox.Show("invalid id/pw\ncheck it and try again ", "Error", MessageBoxButton.OK, MessageBoxImage.Error) == MessageBoxResult.OK)
+                {
+
+                    logining = false;
+
+                }
+                else
+                {
+
+                    this.Close();
+
+                }
+
+            }
+
+        }
+    }
+}
---
+++ TFT/Player.cs
@@ -0,0 +1,92 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+namespace TFT
+{
+    public class Player
+    {
+        public string APIKEY { get; set; }
+
+        private ChromeDriver? driver = null;
+        private ChromeDriverService? service = null;
+        private ChromeOptions? options = null;
+
+        public Player(string id, string pw)
+        {
+
+            DriverInit();
+
+            driver = new ChromeDriver(service, options);
+
+            driver.Navigate().GoToUrl("https://auth.riotgames.com/login#client_id=riot-developer-portal&redirect_uri=https%3A%2F%2Fdeveloper.riotgames.com%2Foauth2-callback&response_type=code&scope=openid%20email%20summoner&ui_locales=en");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            var element = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/div/div/div[2]/div/div/div/div[1]/div/input")); // id
+            element.SendKeys(id);
+
+            element = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/input")); // pw
+            element.SendKeys(pw);
+
+
+            element = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/div/div/button")); // login button
+            element.Click();
+
+            try
+            {
+
+                element = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/div/div/div[2]/div/div/div/span")); // login button
+
+                driver.Dispose();
+
+                throw new PlayerException("wrong id/pw");
+
+            }
+            catch (NoSuchElementException)
+            {
+
+                element = driver.FindElement(By.XPath("//*[@id='apikey']")); // api key
+
+                Console.WriteLine("API KEY : " + element.GetDomProperty("value"));
+
+                APIKEY = element.GetDomProperty("value");
+
+                driver.Dispose();
+
+            }
+            
+
+        }
+
+
+        private void DriverInit()
+        {
+
+            service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+
+            options = new ChromeOptions();
+            options.AddArgument("disable-gpu");
+
+        }
+
+
+    }
+
+    class PlayerException : Exception
+    {
+
+        public PlayerException()
+        {
+        }
+
+        public PlayerException(string message) : base(message)
+        {
+        }
+
+        public PlayerException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+    }
+}
