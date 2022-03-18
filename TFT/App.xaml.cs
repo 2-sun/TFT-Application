a991115b6232e01777fdb5e3e96d69b9063fabed
2-sun <qwerkevin1103@gmail.com>
2022-03-18 18:28:00 +0900

    초기 커밋.

---
+++ TFT/App.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Windows;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TFT
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private ChromeDriver driver = null;
+        private ChromeDriverService service = null;
+        private ChromeOptions options = null;
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Init();
+
+            InputID:
+
+            Console.WriteLine("Riot ID : ");
+            string? id = Console.ReadLine();
+
+            if (id == null) goto InputID;
+
+            InputPW:
+
+            Console.WriteLine("Riot PW : ");
+            string? pw = Console.ReadLine();
+
+            if (pw == null) goto InputPW;
+
+            driver = new ChromeDriver(service, options);
+
+            driver.Navigate().GoToUrl("https://auth.riotgames.com/login#client_id=riot-developer-portal&redirect_uri=https%3A%2F%2Fdeveloper.riotgames.com%2Foauth2-callback&response_type=code&scope=openid%20email%20summoner&ui_locales=en");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+            var element = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/div/div/div[2]/div/div/div/div[1]/div/input")); // id
+            element.SendKeys(id);
+
+            element = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/div/div/div[2]/div/div/div/div[2]/div/input")); // pw
+            element.SendKeys(pw);
+
+            element = driver.FindElement(By.XPath("/html/body/div[2]/div/div/div[2]/div/div/button")); // login button
+            element.Click();
+
+
+        }
+
+        private void Init()
+        {
+
+            service = ChromeDriverService.CreateDefaultService();
+            service.HideCommandPromptWindow = true;
+
+            options = new ChromeOptions();
+            options.AddArgument("enable-gpu");
+
+        }
+    }
+}
